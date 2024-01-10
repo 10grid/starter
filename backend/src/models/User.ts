c96120b9a0761569e0dda971426876449d7d42e6
majidalykhan <majidalykhan@gmail.com>
2024-01-11 00:58:17 +0500

    update: user schema
    update user schema for authentication

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,15 +1,27 @@
 import mongoose from "mongoose";
+const validator = require("validator");
 
 const User = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: true,
+      required: [true, "Please enter your name"],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Please enter your email address"],
       unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [8, "Your password must be at least 8 characters long"],
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
     },
     dateofbirth: {
       type: String,
