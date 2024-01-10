955eee9fd60bb90ce91987ace4b46b76b2aca14f
majidalykhan <majidalykhan@gmail.com>
2024-01-10 15:38:11 +0500

    feat: setup bootstrap
    add bootstrap to react project using npm

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">10grid - Starter Project</div>;
+  return (
+    <div className="App">
+      10grid - Starter Project
+      <div>
+        <Button as="a" variant="primary">
+          Button as link
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
