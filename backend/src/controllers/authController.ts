d508af36d41bbacdb16ec9a86c396a67eb39d7bc
majidalykhan <majidalykhan@gmail.com>
2024-01-15 18:27:40 +0500

    feat: signup user
    signup user using by storing fields like name, email etc and sending jwt in response

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response, NextFunction } from "express";
 import { UserModel } from "../models/User";
+const jwt = require("jsonwebtoken");
 
 exports.signup = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const newUser = await UserModel.create(req.body);
+    const newUser = await UserModel.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+      dateofbirth: req.body.dateofbirth,
+    });
+
+    const token = jwt.sign(
+      { id: newUser._id },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      }
+    );
+
     res.status(201).json({
       status: "success",
+      token,
       data: {
         user: newUser,
       },
